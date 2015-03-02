8c428e8ebe494b8d0a151e70e91523247f0a34ba
gaortiz1 <bisbiridolfo@hotmail.com>
2015-03-01 20:53:14 -0500

    normalizacion base

--- gesso-security-model/src/main/java/ec/com/gesso/security/repository/package-info.java
+++ gesso-security-model/src/main/java/ec/com/gesso/security/repository/package-info.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Un repositorio de media entre las capas de dominio y mapeo de datos, actuando como una colección de objetos de dominio en memoria. Cliente objetos construir especificaciones de consulta de forma declarativa, y someterlos al repositorio de satisfacción. Los objetos se pueden insertar y extraer desde el repositorio, que puedan de una simple colección de objetos, y el código de mapeo encapsulado por el Repositorio 
  */
 /**
  * @author alejandro
