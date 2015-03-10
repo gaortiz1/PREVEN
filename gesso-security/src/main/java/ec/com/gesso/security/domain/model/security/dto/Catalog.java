80844edea20010be8bf18231ff9fd891ecd289d7
maniac787 <maniac787@gmail.com>
2015-03-09 23:43:04 -0500

    actualizacion

--- gesso-security/src/main/java/ec/com/gesso/security/domain/model/security/dto/Catalog.java
+++ gesso-security/src/main/java/ec/com/gesso/security/domain/model/security/dto/Catalog.java
@@ -9,14 +9,12 @@ import java.io.Serializable;
 import java.util.Collection;
 
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
--- gesso-web/src/main/webapp/WEB-INF/views/home.jsp
+++ gesso-web/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,4 @@
 <%-- <jsp:forward page="Contact"></jsp:forward> --%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page session="false"%>
 <!DOCTYPE html>
 <html lang="en">
