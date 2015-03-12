9c3e229a2ca46f6d7806dad9dc3c9dfebb6b27f6
maniac787 <maniac787@gmail.com>
2015-03-11 23:42:51 -0500

    actualizacion pginas

--- gesso-web/src/main/java/ec/com/gesso/controller/administration/CompanyAdministrationController.java
+++ gesso-web/src/main/java/ec/com/gesso/controller/administration/CompanyAdministrationController.java
@@ -1,15 +1,18 @@
 package ec.com.gesso.controller.administration;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import ec.com.gesso.security.domain.model.security.dto.Person;
 import ec.com.gesso.security.domain.model.security.dto.UserDto;
 import ec.com.gesso.security.factory.GessoSecurityFactory;
-
+@Controller
+@SessionAttributes
 public class CompanyAdministrationController {
 	@RequestMapping(value = "/company-administration", method = RequestMethod.GET)
 	public ModelAndView userAdministration(){
