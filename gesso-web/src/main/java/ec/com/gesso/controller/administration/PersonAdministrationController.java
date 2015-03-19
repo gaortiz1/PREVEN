b8d8344b0740e5bb4a21929c9fb13ea4ffeb68d7
maniac787 <maniac787@gmail.com>
2015-03-18 22:09:30 -0500

    creacion formulario persona

--- gesso-web/src/main/java/ec/com/gesso/controller/administration/PersonAdministrationController.java
+++ gesso-web/src/main/java/ec/com/gesso/controller/administration/PersonAdministrationController.java
@@ -1,5 +1,8 @@
 package ec.com.gesso.controller.administration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,7 +18,32 @@ import ec.com.gesso.security.factory.GessoSecurityFactory;
 public class PersonAdministrationController {
 	@RequestMapping(value = "/person-administration", method = RequestMethod.GET)
 	public ModelAndView userAdministration(){
-		return new ModelAndView("person-administration", "command", new Person());
+		
+		Map< String, String > country = new HashMap<String, String>();  
+		country.put("EC", "Ecuador");  
+		country.put("COL", "Colombia");  
+		country.put("URG", "Uruguay");
+		
+		
+		Map< String, String > sex = new HashMap<String, String>();  
+		sex.put("M", "Male");
+		sex.put("F", "Female"); 
+		
+		
+		Map< String, String > levelVulnerability = new HashMap<String, String>();
+		levelVulnerability.put("NIN", "NINGUNA");
+		levelVulnerability.put("EMB", "EMBARAZADA");
+		levelVulnerability.put("DIS", "DISCAPACITADO");
+		levelVulnerability.put("TED", "3RA EDAD");
+		
+		
+        
+		ModelAndView modelAndView = new ModelAndView("person-administration", "command", new Person() );
+		modelAndView.addObject("country", country);
+		modelAndView.addObject("sex", sex);
+		modelAndView.addObject("levelVulnerability",levelVulnerability);
+	        
+		return modelAndView;
 	}
 	
 	@RequestMapping(value = "/person-administration", method = RequestMethod.POST)
