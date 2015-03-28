ad358fe9478225cca9adf91b842d6640f96c1af4
maniac787 <maniac787@gmail.com>
2015-03-27 19:25:21 -0500

    actualizar java script

--- gesso-web/src/main/webapp/resources/gesso/gesso-ajax.js
+++ gesso-web/src/main/webapp/resources/gesso/gesso-ajax.js
@@ -1,18 +1,3 @@
-function cargarProcesos(){
-	$.ajax( "person-fill-ciudad/uno" )
-	.done(function(data) {
-		$.each( data, function( i, item ) {
-			alert( item.catalog_name);
-	   	});
-	})
-	.fail(function() {
-		alert( "error" );
-  	})
-  	.always(function() {
-    	alert( "complete" );
-  	});
-}
-
 function cargarCiudades(elemento){
 	$.ajax( "person-fill-ciudad/uno" )
 		.done(function(data) {
