ef1872870d86c0313cfbb18531961b04d62953ab
maniac787 <maniac787@gmail.com>
2015-04-24 21:43:53 -0500

    actualizaciob
    actualizacion

--- gesso-web/src/main/webapp/resources/gesso/ng-gesso-adm.js
+++ gesso-web/src/main/webapp/resources/gesso/ng-gesso-adm.js
@@ -1,16 +1,19 @@
 var app = angular.module("app-gesso-adm",[]);
-app.controller("app-gesso-ctrl-adm", ['$http', function($http){
+app.controller("app-gesso-ctrl-adm", ['$http', '$scope', function($http, $scope){
+
+    this.controller = $scope;
+
+    this.lstPerson = [];
+
 	this.loadprocess = function(){
 		var response = $http.get('person-edit2.json');
         response.success(function (data, status, headers, config) {
-           alert(data);
+            controller.lstPerson = data;
         });
 
         response.error(function (data, status, headers, config) {
             alert("Error.");
         });
-
-
 	};
 
     this.loadprocess();
