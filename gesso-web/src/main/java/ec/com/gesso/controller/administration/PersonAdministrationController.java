b939362d7b288dd6fa65516e2b7119046a9ae006
maniac787 <maniac787@gmail.com>
2015-05-31 15:47:47 -0500

    Delete innecesary files
    Delete innecesary files

--- gesso-web/src/main/java/ec/com/gesso/controller/administration/PersonAdministrationController.java
+++ gesso-web/src/main/java/ec/com/gesso/controller/administration/PersonAdministrationController.java
@@ -1,18 +1,25 @@
 package ec.com.gesso.controller.administration;
 
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 
+import ec.com.gesso.application.dto.UploadedFile;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
@@ -29,12 +36,19 @@ import ec.com.gesso.model.entity.Process;
 import ec.com.gesso.model.entity.SubProcess;
 import ec.com.gesso.security.factory.GessoSecurityFactory;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @SessionAttributes
 public class PersonAdministrationController {
-	
-	
-	@RequestMapping(value = "/person-administration", method = RequestMethod.GET)
+
+    UploadedFile ufile;
+
+    public PersonAdministrationController() {
+        this.ufile = new UploadedFile();
+    }
+
+    @RequestMapping(value = "/person-administration", method = RequestMethod.GET)
 	public ModelAndView userAdministration(){
 		Collection<CountryDto> country = null;
 		try {
@@ -178,6 +192,51 @@ public class PersonAdministrationController {
 		}
     	return lstJob;
     }
+
+
+	@RequestMapping(value = "/get/{value}", method = RequestMethod.GET)
+	public void get(HttpServletResponse response,@PathVariable String value){
+		try {
+
+			response.setContentType(ufile.type);
+			response.setContentLength(ufile.length);
+			FileCopyUtils.copy(ufile.bytes, response.getOutputStream());
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public @ResponseBody String upload(MultipartHttpServletRequest request, HttpServletResponse response) {
+
+		//0. notice, we have used MultipartHttpServletRequest
+
+		//1. get the file from the request object
+		Iterator<String> itr =  request.getFileNames();
+
+		MultipartFile mpf = request.getFile(itr.next());
+		System.out.println(mpf.getOriginalFilename() +" uploaded!");
+
+		try {
+			ufile.length = mpf.getBytes().length;
+			ufile.bytes= mpf.getBytes();
+			ufile.type = mpf.getContentType();
+			ufile.name = mpf.getOriginalFilename();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//2. send it back to the client as <img> that calls get method
+		//we are using getTimeInMillis to avoid server cached image
+
+		return "<img src='http://localhost:8080/spring-mvc-file-upload/rest/cont/get/"+ Calendar.getInstance().getTimeInMillis()+"' />";
+
+	}
 }
 
 
